37dddd1e16aa17f47b87bd9adc5d7d754b073b3c
dragonfly <212492982@qq.com>
2020-04-13 13:36:42 +0800

    头部

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
+<el-container>
+  <el-header>
+    <div class="logo">
+      <img src="@/assets/logo.png" >
+      <span>后台管理系统</span>
+    </div>
     <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  </el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -14,3 +24,38 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #409EFF;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #0e85fc
+}
+
+.logo {
+
+  img {
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 Vue.prototype.$message = Message
 
@@ -7,3 +7,7 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
